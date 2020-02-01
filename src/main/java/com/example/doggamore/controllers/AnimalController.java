fed94555fe91da03c89defec9d0535cc46c5f3a9
Marcus Wilen <marcus.wilen@hotmail.com>
2020-02-01 15:44:19 +0100

    s

--- src/main/java/com/example/doggamore/controllers/AnimalController.java
+++ src/main/java/com/example/doggamore/controllers/AnimalController.java
@@ -6,19 +6,29 @@ import com.example.doggamore.services.AnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 public class AnimalController {
     @Autowired
     AnimalService  animalService;
 
+    @Autowired
+    AnimalRepository animalRepository;
     @GetMapping("/animals")
     public  Iterable<Animals> getAllAnimals(Model model){
+
         model.addAttribute("allAnimals", animalService.getAnimals());
         return animalService.getAnimals();
+    }
+
+    @PostMapping("/animals")
+    public Animals addAnimals (@RequestBody String id, @RequestBody Map<String, String> body){
+        String name = body.get("animal_name");
+
+        return animalRepository.save(new Animals(1,"dog","this is a dog"));
     }
 
     @GetMapping("/animals/{animal_name}")
