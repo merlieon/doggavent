b3d1753ccb13deee7fc198a0fa3cf402e60846ce
Marcus Wilen <marcus.wilen@hotmail.com>
2020-02-01 15:35:14 +0100

    s

--- src/main/java/com/example/doggamore/controllers/AddAnimalController.java
+++ src/main/java/com/example/doggamore/controllers/AddAnimalController.java
@@ -4,6 +4,7 @@ import com.example.doggamore.models.Animals;
 import com.example.doggamore.repositories.AnimalRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
@@ -13,11 +14,16 @@ public class AddAnimalController {
 
     AnimalRepository animalRepository;
 
-    @GetMapping("/addanimal")
+    @PostMapping("/addanimal")
     public Animals addAnimals (@RequestBody String id, @RequestBody Map<String, String> body){
         String name = body.get("animal_name");
 
         return animalRepository.save(new Animals(1,"dog","this is a dog"));
+    }
+
+    @GetMapping("/addanimal")
+    public String getAnimal(){
+        return "addanimal";
     }
 
 }
