ac560dbbe88a5187b0bac57f179a8f95e3d64a24
Marcus Wilen <marcus.wilen@hotmail.com>
2020-02-01 22:17:04 +0100

    update

--- src/main/java/com/example/doggamore/api/AnimalsControllerApi.java
+++ src/main/java/com/example/doggamore/api/AnimalsControllerApi.java
@@ -3,6 +3,8 @@ package com.example.doggamore.api;
 import com.example.doggamore.models.Animals;
 import com.example.doggamore.services.AnimalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,8 +29,12 @@ public class AnimalsControllerApi {
     }
 
     @GetMapping("/api/animals/{id}")
-    public Animals getAnimalById(@PathVariable int id){
-        return animalService.getAnimalById(id);
+    public ResponseEntity<Animals> getAnimalById(@PathVariable int id){
+        Animals animals = animalService.getAnimalById(id);
+        if (animals == null){
+            return new ResponseEntity<Animals>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Animals>(animals, HttpStatus.OK);
     }
 
 }
