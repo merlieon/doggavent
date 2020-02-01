fe2b6de592973ed142b91e34bd4eea386d473468
Marcus Wilen <marcus.wilen@hotmail.com>
2020-02-01 21:54:20 +0100

    update

---
+++ src/main/java/com/example/doggamore/api/AnimalsControllerApi.java
@@ -0,0 +1,22 @@
+package com.example.doggamore.api;
+
+import com.example.doggamore.models.Animals;
+import com.example.doggamore.services.AnimalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AnimalsControllerApi {
+
+    @Autowired
+    AnimalService animalService;
+
+    @GetMapping("/api/animals")
+    public List<Animals> getAllAnimals(){
+        return animalService.getAnimals();
+    }
+
+}
