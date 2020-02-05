bacd24a73e279369043bd6d5b877bba94f0689e3
Marcus Wilen <marcus.wilen@hotmail.com>
2020-02-05 10:15:45 +0100

    update

--- src/main/java/com/example/doggamore/repositories/AnimalRepository.java
+++ src/main/java/com/example/doggamore/repositories/AnimalRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnimalRepository extends CrudRepository<Animals, Long> {
+public interface AnimalRepository extends CrudRepository<Animals, Integer> {
 }
