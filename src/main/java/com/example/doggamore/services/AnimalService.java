1d07944b61c92749d37b1fcc742abeb4ea259235
Marcus Wilen <marcus.wilen@hotmail.com>
2020-02-05 10:16:07 +0100

    update

--- src/main/java/com/example/doggamore/services/AnimalService.java
+++ src/main/java/com/example/doggamore/services/AnimalService.java
@@ -31,7 +31,7 @@ public class AnimalService {
         for (Animals a : animals()){
             if (a.getAnimal_name().toLowerCase().equals(name)){
                 name = a.getAnimal_name();
-                Long id = a.getId();
+                Integer id = a.getId();
                 String description = a.getAnimal_description();
                 animal = new Animals(id, name, description);
             }
